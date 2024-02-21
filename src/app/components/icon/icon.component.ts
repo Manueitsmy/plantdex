09984bee0299d79da80560d13640f518c1bd4541
manueitsmy <manueitsmy@gmail.com>
2024-02-21 11:01:00 +0100

    9.0 - Icon component + generic name

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -7,21 +7,14 @@ import { Component, Input } from '@angular/core';
 })
 export class IconComponent {
 
-  /*TODO @Input to set icon name
-<app-icon [iconName]="'sun-fill'"></app-icon>
-I wan to dispaliye an icon with a filled sun
+  @Input() iconName: 
+  |'heart' 
+  |'heart-fill'
+  |'sun' 
+  |'sun-fill'
+  |'droplet'
+  |'droplet-fill' = 'heart';
 
-Steps : 
-1- Install bootstrap icon library => done
-2- Inlude icon's CSS file into your Angular project => done
-3- Test if it working with this example :
-<i class="bi-alarm"></i>
-
-4- Adapt to use the @Input value and replace the class
-
-*/
-@Input() iconName: string = ''; 
-
-
+  iconColor = 'orange';
 
 }
